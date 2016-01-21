a768a255cf1cef886cf3ae93c32e898fb5f13457
G D <gdnanaware@gmail.com>
2016-01-21 16:49:32 +0530

    completed assignment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('MyApp',[]);
 app.controller('MyController',function($scope){
     $scope.name = "";
     $scope.onEditChange = "";
-
+    
        
     (function initialize() {
         var mapProp = {
@@ -13,6 +13,7 @@ app.controller('MyController',function($scope){
         $scope.map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
         $scope.geocoder = new google.maps.Geocoder;
         $scope.infowindow = new google.maps.InfoWindow;
+        $scope.mapMarker = new google.maps.Marker({position: $scope.latlng, map: $scope.map});
         
        
     })();
@@ -24,8 +25,7 @@ app.controller('MyController',function($scope){
          $scope.lat= event.latLng.lat();
          $scope.lng= event.latLng.lng();
          $scope.latlng = event.latLng; 
-        $scope.mapMarker = new google.maps.Marker({ position:  $scope.latlng, map: $scope.map});
-       geocodeLatLng($scope.geocoder,$scope.map,$scope.infowindow);
+         geocodeLatLng($scope.geocoder,$scope.map,$scope.infowindow);
         
        
        
@@ -38,22 +38,17 @@ app.controller('MyController',function($scope){
         geocoder.geocode({'location': $scope.latlng}, function(results, status) {
             if (status === google.maps.GeocoderStatus.OK) {
             if (results[1]) {
-                $scope.mapMarker.position =  $scope.latlng;
-                $scope.mapMarker.map = map;
-            /*var marker = new google.maps.Marker({
-              position: $scope.latlng,
-              map: map
-            });*/
-
-            infowindow.setContent(results[1].formatted_address);
                
-              $scope.name = results[1].formatted_address;
-                $scope.temp = "abc";
+               
+                 if ( $scope.mapMarker != null) {
+                     $scope.mapMarker.setMap(null);
+                 }
+                 drawMarker($scope.latlng);
                 
-            console.log($scope.name);
-                
-            infowindow.open(map, $scope.mapMarker);
-            $scope.$digest();
+                 infowindow.setContent(results[1].formatted_address);
+                 infowindow.open(map, $scope.mapMarker);
+                 $scope.name = results[1].formatted_address;
+                 $scope.$digest();
             }
             } 
             
@@ -73,13 +68,28 @@ app.controller('MyController',function($scope){
          geocoder.geocode({'address': address}, function(results, status) {
              if (status === google.maps.GeocoderStatus.OK) {
                  map.setCenter(results[0].geometry.location);
+                 var markpos = results[0].geometry.location;
                  
-                 var marker = new google.maps.Marker({
-                     map: map,
-                     position: results[0].geometry.location
-                 });
+                                  
+                 if ( $scope.mapMarker != null) {
+                     $scope.mapMarker.setMap(null);
+                 }
+                 drawMarker(markpos);
+             
              } 
         });
     
-}
+    }
+ 
+    
+ //---------------------------------Draw Marker-----------------------------------------------------------------------------------    
+    
+    function drawMarker(pos){
+               
+         $scope.mapMarker = new google.maps.Marker({position: pos, map: $scope.map});
+        
+    }
+    
+    
+    
 });
